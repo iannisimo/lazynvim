3823e8860917d297a97f17c7595ab9f8f7f5d7f8
Simone <iannisimo@gmail.com>
2025-04-02 19:37:03 +0200

    add remote-nvim

---
+++ lua/plugins/remote.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "amitds1997/remote-nvim.nvim",
+    version = "*", -- Pin to GitHub releases
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- For standard functions
+      "MunifTanjim/nui.nvim", -- To build the plugin UI
+      "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+    },
+    config = {
+      ssh_config = {
+        ssh_config_file_paths = { "$HOME/.ssh/config", "/etc/ssh/ssh_config" },
+      },
+      remote = {
+        copy_dirs = {
+          config = {
+            compression = {
+              enabled = true,
+              additional_opts = { "--exclude-vcs" },
+            },
+          },
+        },
+      },
+    },
+  },
+}
