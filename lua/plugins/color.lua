8cb7a21600282223d4f7ccb71653630e596b3eee
Simone <iannisimo@gmail.com>
2025-04-02 19:36:43 +0200

    color scheme

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,8 +1,17 @@
+-- return {
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "tokyonight-storm",
+--     },
+--   },
+-- }
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight-storm",
-    },
-  },
+  "baliestri/aura-theme",
+  lazy = false,
+  priority = 1000,
+  config = function(plugin)
+    vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+    vim.cmd([[colorscheme aura-dark]])
+  end,
 }
